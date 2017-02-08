e9ce916f1f9087951877b785975e79bb13d4d185
Thibault Zanini <thibaultzanini@gmail.com>
2017-02-08 17:56:42 -0500

    Fix regression related to f1af1578b043cb1a59135c11bf6d9d5b73d03f0b

--- src/service/section/accounts-section-service.ts
+++ src/service/section/accounts-section-service.ts
@@ -112,7 +112,7 @@ export class AccountsSectionService extends AbstractSectionService {
             self.accountRepository.getUserEmptyList(),
             self.accountRepository.getGroupEmptyList(),
             self.accountRepository.getAccountSystemEmptyList(),
-            self.accountRepository.getDirectoryServicesEmptyList()
+            self.accountRepository.getNewDirectoryServices()
         ]).spread(function (
             users: Array<any>,
             groups: Array<any>,
@@ -122,6 +122,7 @@ export class AccountsSectionService extends AbstractSectionService {
             (users as any)._order = 0;
             (groups as any)._order = 1;
             (system as any)._order = 2;
+            (directoryServices as any)._objectType = 'DirectoryServices';
             (directoryServices as any)._order = 3;
             let entries = [
                 users,
